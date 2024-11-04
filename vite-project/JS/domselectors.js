f7b4729fde0dd62df9a838b5df611b07e749da68
ninac13 <144480538+ninac13@users.noreply.github.com>
2024-11-04 13:58:10 -0500

    11/4/24

---
+++ vite-project/JS/domselectors.js
@@ -0,0 +1,6 @@
+//create cards
+//create theme button
+//create different filters based off of elements in products.js
+const DOMSelectors = {
+
+}
---
+++ vite-project/JS/products.js
@@ -0,0 +1,124 @@
+const ingredients = [
+    {
+      name: "Apple",
+      color: "Red/Green",
+      taste: "Sweet",
+      price: 0.50
+    },
+    {
+      name: "Broccoli",
+      color: "Green",
+      taste: "Bland",
+      price: 1.00
+    },
+    {
+      name: "Salmon",
+      color: "Pink/Orange",
+      taste: "Salty",
+      price: 5.00
+    },
+    {
+      name: "Quinoa",
+      color: "Beige",
+      taste: "Bland",
+      price: 2.00
+    },
+    {
+      name: "Almonds",
+      color: "Brown",
+      taste: "Salty",
+      price: 3.00
+    },
+    {
+      name: "Chicken Breast",
+      color: "White",
+      taste: "Bland",
+      price: 4.00
+    },
+    {
+      name: "Carrots",
+      color: "Orange",
+      taste: "Sweet",
+      price: 0.70
+    },
+    {
+      name: "Brown Rice",
+      color: "Brown",
+      taste: "Bland",
+      price: 1.50
+    },
+    {
+      name: "Yogurt",
+      color: "White",
+      taste: "Sweet",
+      price: 1.20
+    },
+    {
+      name: "Spinach",
+      color: "Dark Green",
+      taste: "Bland",
+      price: 1.00
+    },
+    {
+      name: "Beef",
+      color: "Red",
+      taste: "Salty",
+      price: 6.00
+    },
+    {
+      name: "Eggs",
+      color: "White/Brown",
+      taste: "Bland",
+      price: 0.30
+    },
+    {
+      name: "Sweet Potato",
+      color: "Orange",
+      taste: "Sweet",
+      price: 0.90
+    },
+    {
+      name: "Chickpeas",
+      color: "Beige",
+      taste: "Bland",
+      price: 1.00
+    },
+    {
+      name: "Tomato",
+      color: "Red",
+      taste: "Sweet",
+      price: 0.60
+    },
+    {
+      name: "Oats",
+      color: "Beige",
+      taste: "Bland",
+      price: 0.40
+    },
+    {
+      name: "Blueberries",
+      color: "Blue/Purple",
+      taste: "Sweet",
+      price: 3.50
+    },
+    {
+      name: "Garlic",
+      color: "White",
+      taste: "Spicy",
+      price: 0.50
+    },
+    {
+      name: "Peanut Butter",
+      color: "Brown",
+      taste: "Salty",
+      price: 2.50
+    },
+    {
+      name: "Cucumber",
+      color: "Green",
+      taste: "Bland",
+      price: 0.80
+    }
+  ];
+  
+  console.log(foods);
